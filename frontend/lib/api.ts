8bc6cb701dad67e7886986eb29f150863f99f375
Joaquin Begue <joaquinbegue1204@gmail.com>
2026-03-11 16:01:34 -0300

    Updated fetchProducts function to work in both dev and prod environments

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -1,9 +1,13 @@
-const CLIENT_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000/api";
-const SERVER_API_URL = process.env.API_URL || "http://backend:8000/api";
+export async function fetchProducts({
+  client = false,
+}: { client?: boolean } = {}) {
+  // In dev environment use different urls to fetch from client and server.
+  const API_URL =
+    process.env.DEV && client
+      ? process.env.SERVER_API_URL
+      : process.env.NEXT_PUBLIC_API_URL;
 
-export async function fetchProducts() {
-  const res = await fetch(`${SERVER_API_URL}/products/`, {
+  const res = await fetch(`${API_URL}/products/`, {
     cache: "no-store",
   });
   if (!res.ok) {
